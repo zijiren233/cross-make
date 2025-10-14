438c2951ca1e33518298db5e36ff385121d9d794
zijiren233 <pyh1670605849@gmail.com>
2025-10-15 01:15:44 +0800

    feat: glibc 2.28

--- scripts/build.sh
+++ scripts/build.sh
@@ -157,7 +157,7 @@ function Init() {
         DEFAULT_CONFIG_SUB_REV="a2287c3041a3"
         DEFAULT_GCC_VER="14.3.0"
         DEFAULT_MUSL_VER="1.2.5"
-        DEFAULT_GLIBC_VER="2.36"
+        DEFAULT_GLIBC_VER="2.28"
         DEFAULT_BINUTILS_VER="2.45"
         DEFAULT_GMP_VER="6.3.0"
         DEFAULT_MPC_VER="1.3.1"
