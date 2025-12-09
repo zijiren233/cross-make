2880faf7bd1cec01060eb2576f985137f1037df7
zijiren233 <pyh1670605849@gmail.com>
2025-12-09 23:21:16 +0800

    fix: risvc64

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,11 +21,11 @@ function GetYamlDefault() {
 function GetTargetConfig() {
     local id_or_target="$1"
     local key="$2"
-    # First try to find by ID, then by TARGET
+    # First try to find by ID, then by TARGET (use first match only)
     local result
-    result=$(yq -r ".targets[] | select(.ID == \"${id_or_target}\") | .${key} // \"\"" "$TARGETS_YAML")
+    result=$(yq -r "first(.targets[] | select(.ID == \"${id_or_target}\")) | .${key} // \"\"" "$TARGETS_YAML")
     if [ -z "$result" ]; then
-        result=$(yq -r ".targets[] | select(.TARGET == \"${id_or_target}\") | .${key} // \"\"" "$TARGETS_YAML")
+        result=$(yq -r "first(.targets[] | select(.TARGET == \"${id_or_target}\")) | .${key} // \"\"" "$TARGETS_YAML")
     fi
     echo "$result"
 }
