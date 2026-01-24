63bd92121d4814c423d85e46c12c9aafa9867d32
zijiren233 <pyh1670605849@gmail.com>
2026-01-24 11:58:50 +0800

    feat: glibc 2.43 and backport glibc cve patches

--- scripts/update-glibc-patches.sh
+++ scripts/update-glibc-patches.sh
@@ -12,7 +12,7 @@ TMP_DIR="/tmp/glibc-patches-$$"
 UPSTREAM_REPO="https://sourceware.org/git/glibc.git"
 DEBIAN_REPO="https://salsa.debian.org/glibc-team/glibc.git"
 
-GLIBC_VERSIONS="2.27 2.28 2.29 2.30 2.31 2.32 2.33 2.34 2.35 2.36 2.37 2.38 2.39 2.40 2.41 2.42"
+GLIBC_VERSIONS="2.27 2.28 2.29 2.30 2.31 2.32 2.33 2.34 2.35 2.36 2.37 2.38 2.39 2.40 2.41 2.42 2.43"
 
 cleanup() {
     if [ -d "$TMP_DIR" ]; then
